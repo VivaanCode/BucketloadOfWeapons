d685b6b16292f25bf832c4e34fa541cab6e8cd2a
Vivaan <vivaan@vivaan.dev>
2026-05-09 14:03:31 -0700

    init2

---
+++ src/client/java/dev/vivaan/bucketloadofweapons/client/BucketloadOfWeaponsClient.java
@@ -0,0 +1,10 @@
+package dev.vivaan.bucketloadofweapons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BucketloadOfWeaponsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/dev/vivaan/bucketloadofweapons/BucketloadOfWeaponsDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.vivaan.bucketloadofweapons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BucketloadOfWeaponsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
