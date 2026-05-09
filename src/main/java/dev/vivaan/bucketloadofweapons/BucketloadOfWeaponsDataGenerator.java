d30ce1c262893af458cf516a201f7d01909b7948
Vivaan <vivaan@vivaan.dev>
2026-05-09 14:32:39 -0700

    updated a bunch of json values and it took 5 years to finally look good

--- src/main/java/dev/vivaan/bucketloadofweapons/BucketloadOfWeaponsDataGenerator.java
+++ src/main/java/dev/vivaan/bucketloadofweapons/BucketloadOfWeaponsDataGenerator.java
@@ -1,11 +1,14 @@
 package dev.vivaan.bucketloadofweapons;
 
+import dev.vivaan.bucketloadofweapons.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class BucketloadOfWeaponsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
